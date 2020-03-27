9742dccdbbd0494bb9945fa50e2d2127ba304e2d
asoto <alejandro.soto@bibamgroup.com>
2020-03-27 13:11:41 -0300

    validaciones en precios

--- src/components/Map/MarkerComponent.js
+++ src/components/Map/MarkerComponent.js
@@ -1,20 +1,16 @@
 import React from 'react'
 
 export default function MarkerComponent({data, logo, handleClickOpenInfo}) {
-    const precios = Object.values(data.precios)[0];
+    let precios = Object.values(data.precios)[0].precio;
+    precios = precios.toString().slice(0,5);
+    
     return (
          <section onClick={()=>{handleClickOpenInfo(data)}}>
              <div className="markerComponent">
-                 <div className="price">{precios.precio}</div>
+                 <div className="price">{precios}</div>
              </div>
          </section>
 
 
     )
 }
-//<section onClick={()=>{handleClickOpenInfo(data)}}>
-//<div className="markerComponent">
-//    <img alt="logo" src={logo}/>
-//    <b>{precios.precio}</b>
-//</div>
-//</section>
